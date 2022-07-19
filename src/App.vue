9f2e7e95603dbfa6a57b076b673666c629aa33a1
Hung <tdd057@yahoo.com.tw>
2022-07-19 22:52:34 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <h1 class = "display-1 text-bold"> My App </h1> 
+  <h1 class = "text-gray-soft text-regular"> Greetings to you all. </h1> 
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
